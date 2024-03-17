9d4c665532b8d494ebf8a7b08f343d4831283e81
Mykola Fiantsev <to.bigmotor@gmail.com>
2024-03-17 20:51:11 +0200

    UserDefaults codable extension can be used on any UserDefaults eg standard, shared, ...

--- Sources/Helpers/Extensions/UserDefaultsExtension.swift
+++ Sources/Helpers/Extensions/UserDefaultsExtension.swift
@@ -10,15 +10,15 @@ import Foundation
 public extension UserDefaults {
   func setCodable<T: Codable>(_ value: T, for key: String) {
     let data = try? JSONEncoder().encode(value)
-    UserDefaults.standard.set(data, forKey: key)
+    set(data, forKey: key)
   }
   
   func codable<T: Codable>(for key: String) -> T? {
-    let data = UserDefaults.standard.data(forKey: key)
+    let data = data(forKey: key)
     return data.flatMap { try? JSONDecoder().decode(T.self, from: $0) }
   }
   
   func deleteValue(key: String) {
-    UserDefaults.standard.set(nil, forKey: key)
+    set(nil, forKey: key)
   }
 }
